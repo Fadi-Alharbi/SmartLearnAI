95d6a922d1d2834361cd08cb996ff482b262d5f2
iron <fadialhrby0@gmail.com>
2025-10-25 21:15:11 +0300

    first commit

---
+++ Test.py
@@ -0,0 +1,6 @@
+<div class="container">
+        <div class="upload-box" id="uploadBox">
+            <div class="upload-label">Upload your file</div>
+            <div class="file-name" id="fileName"></div>
+            <input type="file" class="file-input" id="fileInput" accept=".pdf,.doc,.docx,.txt">
+        </div>
